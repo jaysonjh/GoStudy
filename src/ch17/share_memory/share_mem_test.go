d41ef2435ae8ab7a084d4b8e02188dff8527f7fe
江鸿 <jayson@zhiyesoft.com>
2019-11-20 14:12:26 +0800

    feat(all): 共享内存并发教程

---
+++ src/ch17/share_memory/share_mem_test.go
@@ -0,0 +1,56 @@
+package share_memory
+
+import (
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestCounter(t *testing.T) {
+	counter := 0
+	for i := 0; i < 5000; i++ {
+		go func() {
+			counter++
+		}()
+	}
+	time.Sleep(time.Millisecond * 50)
+	t.Logf("Counter = %d", counter)
+}
+
+func TestCounterThreadSafe(t *testing.T) {
+	var mut sync.Mutex
+	counter := 0
+	for i := 0; i < 5000; i++ {
+		go func() {
+			defer func() {
+				mut.Unlock()
+			}()
+			mut.Lock()
+			counter++
+		}()
+	}
+	time.Sleep(time.Millisecond * 50)
+	t.Logf("Counter = %d", counter)
+}
+
+func TestCounterWaitGroup(t *testing.T) {
+	var mut sync.Mutex
+	var wg sync.WaitGroup
+	counter := 0
+	for i := 0; i < 5000; i++ {
+		/// 新增协程到group中
+		wg.Add(1)
+		go func() {
+			defer func() {
+				mut.Unlock()
+			}()
+			mut.Lock()
+			counter++
+			wg.Done()
+		}()
+	}
+	// time.Sleep(time.Millisecond * 50)
+	/// 等待所有的协程完成后执行后续操作
+	wg.Wait()
+	t.Logf("Counter = %d", counter)
+}
