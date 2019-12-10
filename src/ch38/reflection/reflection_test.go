d83666015678c7df12e5a25eba771ff3c79b0148
江鸿 <jayson@zhiyesoft.com>
2019-12-10 21:26:51 +0800

    fix(all): 完成反射部分代码

--- src/ch38/reflection/reflection_test.go
+++ src/ch38/reflection/reflection_test.go
@@ -30,11 +30,32 @@ func TestBasicType(t *testing.T) {
 }
 
 /// 放射的具体用法
-
 type Employ struct {
 	EmployID string
-	Name string `format:"normal"`
-	Age int
+	Name     string `format:"normal"`
+	Age      int
 }
 
-func (e *Employ)
+func (e *Employ) UpdateAge(age int) {
+	e.Age = age
+}
+
+type Custmer struct {
+	CookieID string
+	Name     string
+	Age      int
+}
+
+func TestInvokeByName(t *testing.T) {
+	e := &Employ{"1", "Mike", 30}
+	t.Logf("Name: value(%[1]v), Type(%[1]T)", reflect.ValueOf(*e).FieldByName("Name"))
+
+	if nameField, ok := reflect.TypeOf(*e).FieldByName("Name"); !ok {
+		t.Error("Failed to get 'Name' field")
+	} else {
+		t.Logf("Tag: %T", nameField.Tag.Get("format"))
+	}
+
+	reflect.ValueOf(e).MethodByName("UpdateAge").Call([]reflect.Value{reflect.ValueOf(1)})
+	t.Log("Update Age", e)
+}
